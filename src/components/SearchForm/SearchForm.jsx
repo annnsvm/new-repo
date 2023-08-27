328680a71f564c84e7428b76725eefc33ef2451c
Oleksii-Senchenko <106326629+Oleksii-Senchenko@users.noreply.github.com>
2023-08-27 11:45:19 +0300

    done country search

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
 const regions = [
   { id: 'africa', value: 'africa', name: 'Africa' },
@@ -9,13 +10,25 @@ const regions = [
   { id: 'oceania', value: 'oceania', name: 'Oceania' },
 ];
 
-export const SearchForm = () => {
+export const SearchForm = ({ getCountryValue }) => {
+  const [country, setCountry] = useState('');
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    getCountryValue(country);
+  };
+
+  const handleInput = ({ target }) => {
+    setCountry(target.value);
+  };
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled onSubmit={handleSubmit}>
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
+      <Select aria-label="select" name="region" required onChange={handleInput}>
         <option selected disabled defaultValue="">
           Select a region and press enter
         </option>
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,12 +6,46 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { fetchByRegion } from 'service/country-service';
 
 export const CountrySearch = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [countries, setСountries] = useState([]);
+  const [params, setParams] = useSearchParams();
+
+  const getCountryValue = value => {
+    setParams({ region: value });
+  };
+
+  useEffect(() => {
+    const region = params.get('region');
+
+    if (!region) {
+      return;
+    }
+    setIsLoading(true);
+
+    const fetchSelectedRegion = async () => {
+      try {
+        const response = await fetchByRegion(region);
+        setСountries(response);
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchSelectedRegion();
+  }, [params]);
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
+        {isLoading && <Loader />}
+        <SearchForm getCountryValue={getCountryValue} />
+        <CountryList countries={countries} />
       </Container>
     </Section>
   );
