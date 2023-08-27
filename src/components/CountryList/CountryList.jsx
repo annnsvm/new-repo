d79301c7b01aed87c55fc67f09d8a0999facd9fa
Anna Samoilova <samoilova0299@gmail.com>
2023-08-27 10:06:21 +0200

    add list

--- src/components/CountryList/CountryList.jsx
+++ src/components/CountryList/CountryList.jsx
@@ -1,5 +1,17 @@
-import { Grid, GridItem } from 'components';
+import { Grid, GridItem } from "components";
+import { Link } from "react-router-dom";
+import { routes } from "../../routes";
 
 export const CountryList = ({ countries }) => {
-  return <h2>CountryList</h2>;
+  return (
+    <Grid>
+      {countries.map((country) => (
+        <GridItem key={country.id}>
+          <Link to={`${routes.COUNTRY}/${country.id}`}>
+            <img src={country.flag} alt={country.country} />
+          </Link>
+        </GridItem>
+      ))}
+    </Grid>
+  );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
-import { Container, CountryList, Heading, Loader, Section } from 'components';
+import { Container, CountryList, Heading, Loader, Section } from "components";
+import { useEffect, useState } from "react";
+import { getCountries } from "../service/country-service";
 
 export const Home = () => {
+  const [allCountries, setAllCountries] = useState([]);
+
+  useEffect(() => {
+    const getAllCountries = async () => {
+      try {
+        const response = await getCountries();
+        setAllCountries(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllCountries();
+  }, []);
+
   return (
     <Section>
       <Container>
         <h2>Home</h2>
+        <CountryList countries={allCountries} />
       </Container>
     </Section>
   );
